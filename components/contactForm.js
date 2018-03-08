92ed20db3b55e4a56658a41e094983f306281c77
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-03-08 11:22:37 +0000

    first commit

---
+++ components/contactForm.js
@@ -0,0 +1,66 @@
+var contacts = [
+  {
+    id: 1,
+    firstName: 'Jan',
+    lastName: 'Nowak',
+    email: 'jan.nowak@example.com',
+  },
+  {
+    id: 2,
+    firstName: 'Adam',
+    lastName: 'Kowalski',
+    email: 'adam.kowalski@example.com',
+  },
+  {
+    id: 3,
+    firstName: 'Zbigniew',
+    lastName: 'Koziol',
+    email: 'zbigniew.koziol@example.com',
+  }
+];
+
+var contactForm = {
+  firstName: '',
+  lastName: '',
+  email: ''
+};
+
+var App = React.createClass({
+  render: function() {
+    return (
+      React.createElement('div', {className: 'app'},
+        React.createElement(ContactForm, {contact: contactForm}),
+        React.createElement(Contacts, {items: contacts}, {})
+      )
+    );
+  }
+});
+
+var ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  },
+
+  render: function() {
+    return (
+      React.createElement('form', {className: 'contactForm'},
+        React.createElement('input', {
+          type: 'text',
+          placeholder: 'Imię',
+          value: this.props.contact.firstName,
+        }),
+        React.createElement('input', {
+          type: 'text',
+          placeholder: 'Nazwisko',
+          value: this.props.contact.lastName,
+        }),
+        React.createElement('input', {
+          type: 'email',
+          placeholder: 'Email',
+          value: this.props.contact.email,
+        }),
+        React.createElement('button', {type: 'submit'}, "Dodaj kontakt")
+      )
+    )
+  },
+})
